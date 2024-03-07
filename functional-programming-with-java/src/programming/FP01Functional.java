5d4efb663dc2bc678f4379a730302ff45763b788
joaorobertoo <jroberto.p@hotmail.com>
2024-03-06 22:47:46 -0300

    Exercises: 1. Print odd numbers 2. Print Courses 3. Print Spring course only 4. Print courses size 4 or more

--- functional-programming-with-java/src/programming/FP01Functional.java
+++ functional-programming-with-java/src/programming/FP01Functional.java
@@ -6,16 +6,38 @@ public class FP01Functional {
 
     public static void main(String[] args){
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-
-       // printAllNumberInListFunctional(numbers);
-            printEvenNumbersInListFunctional(numbers);
+        List<String> courses = List.of("Java", "Python", "AWS", "Azure", "Spring", "C#");
+        //printAllNumberInListFunctional(numbers);
+        //printEvenNumbersInListFunctional(numbers);
+        //printOddNumbersInListFunctional(numbers);
+        //printCourses(courses);
+        //printSpringCourse(courses);
+        printCoursesSizeEqualsOrGreaterThanFour(courses);
     }
 
-    private static void printAllNumberInListFunctional(List<Integer> numbers) {
-        //convert list of numbers into stream
-        //Method reference
-        numbers.forEach(System.out::println);
+    private static void printCoursesSizeEqualsOrGreaterThanFour(List<String> courses) {
+        courses.stream()
+                .filter(course->course.length()>=4)
+                .forEach(System.out::println);
     }
+
+    private static void printSpringCourse(List<String> courses) {
+        courses.stream().filter(course->course.equals("Spring")).forEach(System.out::println);
+    }
+
+    private static void printCourses(List<String> courses) {
+        courses.stream().forEach(System.out::println);
+    }
+
+    private static void printOddNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream().filter(number -> number%2==1).forEach(System.out::println);
+    }
+
+//    private static void printAllNumberInListFunctional(List<Integer> numbers) {
+//        //convert list of numbers into stream
+//        //Method reference
+//        numbers.forEach(System.out::println);
+//    }
 
 //    private static boolean isEven(int number){
 //        return number%2==0;
