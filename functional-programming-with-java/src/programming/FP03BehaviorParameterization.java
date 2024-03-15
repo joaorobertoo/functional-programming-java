793fc0b5cc40959c337f3d4db8975b2d7bd2f8ca
joaorobertoo <jroberto.p@hotmail.com>
2024-03-14 22:04:28 -0300

    Supplier and UnaryOperator

--- functional-programming-with-java/src/programming/FP03BehaviorParameterization.java
+++ functional-programming-with-java/src/programming/FP03BehaviorParameterization.java
@@ -1,8 +1,8 @@
 package programming;
 
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
@@ -20,9 +20,31 @@ public class FP03BehaviorParameterization {
         //exercise, writing a behaviorParameterization to do square of a numbers
         //possible to pass functions as parameters
         //variables with logic of a function
-        List<Integer> squaredNumbers = getCollect(numbers, x -> x * x);
+        //List<Integer> squaredNumbers = getCollect(numbers, x -> x * x);
 
-        squaredNumbers.stream().forEach(System.out::println);
+        //squaredNumbers.stream().forEach(System.out::println);
+
+        //Types of Functional interfaces
+        //Predicate has input and boolean output
+        Predicate<Integer> isEvenPredicate = x->x%2==0;
+        //Function has input and output
+        Function<Integer,Integer> squareFunction = x-> x*x;
+        //Consumer has input but no output
+        Consumer<Integer> sysoutConsumer = x->System.out.println(x);
+        //Binary operator add numbers in
+        BinaryOperator<Integer> sumBinaryOperator = (x,y)->x+y;
+        //Supplier no input -> return something(factory pattern)
+        Supplier<Integer> randomIntegerSupplier = ()-> {
+            Random random = new Random();
+            return random.nextInt(1000);
+        };
+        //System.out.println(randomIntegerSupplier.get());
+
+        //UnaryOperator -> takes one parameter and returns result of same type
+        UnaryOperator<Integer> unaryOperator = (x) -> 3*x;
+
+        System.out.println(unaryOperator.apply(10));
+
 
     }
 
